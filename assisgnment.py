5e1b5b3abecdb62cdb87bd2cf7af5c622ad9e3e3
losirlu1411 <148865607+losirlu1411@users.noreply.github.com>
2023-12-08 23:21:55 +0000

    update different for correlation heat maps
    update different  data set for correlation heat maps  and updated countries for bar graph

--- assisgnment.py
+++ assisgnment.py
@@ -16,8 +16,8 @@ def read_data(filename):
     # Set the index and select specific countries and years
     data1.index = data1.iloc[:, 0]
     data1 = data1.iloc[:, 1:]
-    countries = ["China", "India", "Russian Federation","Brazil","Indonesia", "United States"]
-    years = ['1990', '1995', '2000', '2005'] # all all tghe year like 1990 to 2008
+    countries = ["China", "India", "Russian Federation","Brazil","Indonesia", "United States","South Africa"]
+    years = ['1990','1991', '1992','1993','1994','1995','1996','1997','1998','1999' ,'2000','2001', '2002', '2003', '2004','2005']
     data1 = data1.loc[countries, years]
 
     # Transpose the data for easier plotting
@@ -27,6 +27,7 @@ def read_data(filename):
 
 def  bar(df,xlbl,ylbl,title):
     years = [1990, 1995, 2000, 2005]
+
     df_filter=df.loc[years,:]
     plt.figure()
     x = np.arange(len(df_filter.columns))  # the label locations
@@ -37,7 +38,7 @@ def  bar(df,xlbl,ylbl,title):
     fig, ax = plt.subplots(layout='constrained')
 
 
-    for ii in df.index:
+    for ii in df_filter.index:
         offset = width * multiplier
         reacts = ax.bar(x + offset, df_filter.loc[ii].values.flatten(), width, label=ii)
         #rects = ax.bar(x + offset, measurement, width, label=attribute)
@@ -54,16 +55,19 @@ def  bar(df,xlbl,ylbl,title):
 
     plt.show()
 
-def heat(country,df1,df2,df3):
+def heat(country,df1,df2,df3,df4,df5,df6):
     dt_corr = pd.DataFrame()
-    dt_corr["Agriculture Methane"] = df2.loc[country,:].values
-    dt_corr["Urban Population"] = df2.loc[country,:].values
     dt_corr["Methane (KT)"] = df1.loc[country,:].values
-
+    dt_corr["Agriculture Methane"] = df2.loc[country,:].values
+    dt_corr["Urban Population"] = df3.loc[country,:].values
+    dt_corr["Agriculture Land"] = df4.loc[country,:].values
+    dt_corr["Arable land"] = df5.loc[country,:].values
+    dt_corr["Forest Land"] = df6.loc[country,:].values
+    
     corr = dt_corr.corr().round(3)
     print(corr)
     plt.figure()
-    grid = plt.imshow(corr,cmap='Accent_r')
+    plt.imshow(corr,cmap='Accent_r')
     plt.colorbar()
     plt.xticks(np.arange(len(corr.columns)), labels=corr.columns, rotation=90)
     plt.yticks(np.arange(len(corr.columns)), labels=corr.columns)
@@ -80,11 +84,14 @@ def heat(country,df1,df2,df3):
 data, data_t =  read_data("API_EN.ATM.METH.KT.CE_DS2_en_csv_v2_5995564.csv")
 data2, data2_t =  read_data("API_EN.ATM.METH.AG.ZS_DS2_en_csv_v2_5995573.csv")
 data3, data3_t =  read_data("API_SP.URB.TOTL.IN.ZS_DS2_en_csv_v2_5996759.csv")
+data4, data4_t =  read_data("API_AG.LND.AGRI.ZS_DS2_en_csv_v2_5995314.csv")
+data5, data5_t =  read_data("API_AG.LND.ARBL.ZS_DS2_en_csv_v2_5995308.csv")
+data6, data6_t =  read_data("API_AG.LND.FRST.ZS_DS2_en_csv_v2_5994693.csv")
 
 
 bar(data_t,'Years','Methane Emission (KT)','Methane Emission by Countries and Years' )
 bar(data2_t,'Years','Agriculture Methane Emission (KT)',\
     'Agriculture Methane Emission by Countries and Years')
-heat("India",data,data2,data3)
-heat("Brazil",data,data2,data3)
-heat("China",data,data2,data3)
+heat("India",data,data2,data3,data4,data5,data6)
+heat("Brazil",data,data2,data3,data4,data5,data6)
+heat("China",data,data2,data3,data4,data5,data6)
